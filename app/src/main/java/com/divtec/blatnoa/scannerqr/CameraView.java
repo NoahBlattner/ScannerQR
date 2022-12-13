4b7e0a89f3052447b5644d2f3d490df59eab4eb7
blatnoa <noahblattner04@gmail.com>
2022-12-13 16:30:14 +0100

    implémentation de cameraX (ça à tout fait explosé...)

--- app/src/main/java/com/divtec/blatnoa/scannerqr/CameraView.java
+++ app/src/main/java/com/divtec/blatnoa/scannerqr/CameraView.java
@@ -1,20 +1,12 @@
 package com.divtec.blatnoa.scannerqr;
 
 import android.content.Context;
-import android.graphics.Camera;
-import android.hardware.camera2.CameraDevice;
 import android.util.AttributeSet;
-import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-import androidx.annotation.NonNull;
+public class CameraView extends SurfaceView {
 
-public class CameraView extends SurfaceView  {
-
-    public CameraView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-
-        setBackgroundColor(0xFF000000);
+    public CameraView(Context context) {
+        super(context);
     }
-
 }
--- app/src/main/java/com/divtec/blatnoa/scannerqr/QrScanner.java
+++ app/src/main/java/com/divtec/blatnoa/scannerqr/QrScanner.java
@@ -1,26 +1,57 @@
 package com.divtec.blatnoa.scannerqr;
 
-import android.hardware.camera2.CameraDevice;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
 
+import com.google.common.util.concurrent.ListenableFuture;
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
 import com.google.mlkit.vision.barcode.common.Barcode;
 
-public class QrScanner extends AppCompatActivity {
+import java.util.concurrent.ExecutionException;
+
+public class QrScanner<PreviewView> extends AppCompatActivity {
     private BarcodeScannerOptions options = new BarcodeScannerOptions.Builder()
             .setBarcodeFormats(Barcode.FORMAT_QR_CODE).build();
 
-    private CameraView cameraView;
+    private PreviewView previewView;
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cameraView = new CameraView(this, null);
+        previewView = findViewById(R.id.previewView);
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
 
-        System.out.println("QWERTZ" + cameraView);
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                bindPreview(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    private void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        Preview preview = new Preview.Builder().build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this , cameraSelector, preview);
     }
 }
